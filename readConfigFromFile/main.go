9885731c04bbf16d5015604070308c8a5f4b29e8
Tuan Pham <undivert@gmail.com>
2022-12-14 11:48:35 +0800

    upgrade code

--- readConfigFromFile/main.go
+++ readConfigFromFile/main.go
@@ -10,37 +10,40 @@ import (
 
 // Configuration represents the structure of our configuration file
 type Configuration struct {
-  Port int    `yaml:"port" env:"PORT"`
-  Host string `yaml:"host" env:"HOST"`
+  Port int `yaml:"port"`
+  Host string `yaml:"host"`
 }
 
-// ReadConfig reads the configuration from a YAML file and environment variables,
-// and returns a Configuration struct
+// ReadConfig reads the configuration from a YAML file and returns a Configuration struct
 func ReadConfig(filename string) (Configuration, error) {
-  // Initialize a new Configuration struct
-  var config Configuration
-
-  // Read the configuration from the YAML file
+  // Load the configuration file into a byte slice
   configData, err := ioutil.ReadFile(filename)
   if err != nil {
     return Configuration{}, err
   }
+
+  // Initialize a new Configuration struct
+  var config Configuration
+
+  // Unmarshal the YAML data into the Configuration struct
   err = yaml.Unmarshal(configData, &config)
   if err != nil {
     return Configuration{}, err
   }
 
-  // Read the configuration from environment variables
-  err = envdecode.Decode(&config)
-  if err != nil {
-    return Configuration{}, err
+  // Check for environment variables and use them to override the YAML configuration
+  if port := os.Getenv("PORT"); port != "" {
+    config.Port, _ = strconv.Atoi(port)
+  }
+  if host := os.Getenv("HOST"); host != "" {
+    config.Host = host
   }
 
   return config, nil
 }
 
 func main() {
-  // Read the configuration from the YAML file and environment variables
+  // Read the configuration from the YAML file
   config, err := ReadConfig("config.yml")
   if err != nil {
     log.Fatal(err)
